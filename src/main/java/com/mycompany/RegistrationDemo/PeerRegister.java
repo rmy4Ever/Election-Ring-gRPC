4d3dde6236a24cf22916d889a6f1e8d4c7bb88ff
rmy4Ever <116610500+rmy4Ever@users.noreply.github.com>
2025-10-24 19:59:25 +1200

    Update PeerRegister.java

--- src/main/java/com/mycompany/RegistrationDemo/PeerRegister.java
+++ src/main/java/com/mycompany/RegistrationDemo/PeerRegister.java
@@ -4,10 +4,6 @@
  */
 package com.mycompany.RegistrationDemo;
 
-/**
- *
- * @author Kevin
- */
 import com.mycompany.RegistrationDemo.proto.Neighbor;
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
