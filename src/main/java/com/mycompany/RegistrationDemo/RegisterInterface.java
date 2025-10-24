a9874c248940049aa260281da2a005911a98c036
rmy4Ever <116610500+rmy4Ever@users.noreply.github.com>
2025-10-24 20:05:44 +1200

    RegisterInterface.java

--- src/main/java/com/mycompany/RegistrationDemo/RegisterInterface.java
+++ src/main/java/com/mycompany/RegistrationDemo/RegisterInterface.java
@@ -2,6 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+/*
+*Kevin Bolanavatu - S11198128
+*Maria Waqairatavo - S11174090
+*Salote Katia - S11196202
+*/
+
 package com.mycompany.RegistrationDemo;
 
 import com.mycompany.RegistrationDemo.proto.Neighbor;
